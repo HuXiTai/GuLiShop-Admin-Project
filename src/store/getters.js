958c9d64cc9b1ba5e959251a560625bfd54f50a5
HuXiTai <2449258269@qq.com>
2022-03-02 20:03:19 +0800

    3-2-pm-重写vuex里的异步请求以及token的存储方式，实现权限控制(优化)

--- src/store/getters.js
+++ src/store/getters.js
@@ -3,6 +3,9 @@ const getters = {
   device: state => state.app.device,
   token: state => state.user.token,
   avatar: state => state.user.avatar,
-  name: state => state.user.name
-}
-export default getters
+  name: state => state.user.name,
+  routes: state => state.user.routes,
+  buttons: state => state.user.buttons,
+  roles: state => state.user.roles
+};
+export default getters;
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,28 @@
 import { login, logout, getInfo } from "@/api/user";
 // import { getToken, setToken, removeToken } from "@/utils/auth";
-import { resetRouter } from "@/router";
+import {
+  resetRouter,
+  constantRoutes,
+  allAsyncRoutes,
+  anyRoutes,
+  default as router
+} from "@/router";
+
+//通过权限信息过滤出路由的函数
+function filtersAllAsyncRoutes(allAsyncRoutes, routes) {
+  const asyncRoutes = allAsyncRoutes.filter(item => {
+    if (routes.includes(item.name)) {
+      if (item.children && item.children.length) {
+        item.children = filtersAllAsyncRoutes(item.children, routes);
+      }
+      return true;
+    }
+  });
+
+  return asyncRoutes;
+}
+
+import cloneDeep from "lodash/cloneDeep";
 
 const state = {
   token: localStorage.getItem("token_key"),
@@ -24,9 +46,11 @@ const actions = {
   async getInfo({ commit, state }) {
     const re = await getInfo(state.token);
     if (re.code === 20000 || re.code === 200) {
-      const { name, avatar } = re.data;
-      commit("SET_NAME", name);
-      commit("SET_AVATAR", avatar);
+      commit("SET_USERINFO", re.data);
+      commit(
+        "SET_ROUTES",
+        filtersAllAsyncRoutes(cloneDeep(allAsyncRoutes), re.data.routes)
+      );
       return "ok";
     } else {
       return Promise.reject(new Error("error"));
@@ -56,15 +80,25 @@ const mutations = {
     state.token = "";
     state.name = "";
     state.avatar = "";
+    state.buttons = [];
+    state.roles = [];
+    state.routes = [];
   },
   SET_TOKEN: (state, token) => {
     state.token = token;
   },
-  SET_NAME: (state, name) => {
-    state.name = name;
+
+  SET_USERINFO: (state, userInfo) => {
+    state.name = userInfo.name;
+    state.avatar = userInfo.avatar;
+    state.buttons = userInfo.buttons;
+    state.roles = userInfo.roles;
   },
-  SET_AVATAR: (state, avatar) => {
-    state.avatar = avatar;
+
+  SET_ROUTES: (state, asyncRoutes) => {
+    state.routes = [...constantRoutes, ...asyncRoutes, anyRoutes];
+
+    router.addRoutes([...asyncRoutes, anyRoutes]);
   }
 };
 
