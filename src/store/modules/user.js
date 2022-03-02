8ea9bac35dde049adb836b3bf73a29610f5e4839
HuXiTai <2449258269@qq.com>
2022-03-02 18:47:27 +0800

    3-2-pm-user请求的重写以及token保存的重写

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,134 +1,61 @@
 import { login, logout, getInfo } from "@/api/user";
-import { getToken, setToken, removeToken } from "@/utils/auth";
+// import { getToken, setToken, removeToken } from "@/utils/auth";
 import { resetRouter } from "@/router";
 
-const getDefaultState = () => {
-  return {
-    token: getToken(),
-    name: "",
-    avatar: ""
-  };
+const state = {
+  token: localStorage.getItem("token_key"),
+  name: "",
+  avatar: ""
 };
 
-const state = getDefaultState();
-
 const actions = {
-  // user login
-  // login({ commit }, userInfo) {
-  //   const { username, password } = userInfo;
-  //   return new Promise((resolve, reject) => {
-  //     login({ username: username.trim(), password: password })
-  //       .then(response => {
-  //         const { data } = response;
-  //         commit("SET_TOKEN", data.token);
-  //         setToken(data.token);
-  //         resolve();
-  //       })
-  //       .catch(error => {
-  //         reject(error);
-  //       });
-  //   });
-  // },
-
-  // user login 的async/await写法
-  async login(miniStore, userInfo) {
-    try {
-      const { username, password } = userInfo;
-      const re = await login({ username: username.trim(), password: password });
-      const { data } = re;
-      miniStore.commit("SET_TOKEN", data.token);
-      setToken(data.token);
+  async login({ commit }, userInfo) {
+    const { username, password } = userInfo;
+    const re = await login({ username: username.trim(), password: password });
+    if (re.code === 20000 || re.code === 200) {
+      commit("SET_TOKEN", re.data.token);
+      localStorage.setItem("token_key", re.data.token);
       return "ok";
-    } catch (e) {
-      return Promise.reject(e);
+    } else {
+      return Promise.reject(new Error("error"));
     }
   },
 
-  // get user info
-  getInfo({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      getInfo(state.token)
-        .then(response => {
-          const { data } = response;
-
-          if (!data) {
-            return reject("Verification failed, please Login again.");
-          }
-
-          const { name, avatar } = data;
-
-          commit("SET_NAME", name);
-          commit("SET_AVATAR", avatar);
-          resolve(data);
-        })
-        .catch(error => {
-          reject(error);
-        });
-    });
+  async getInfo({ commit, state }) {
+    const re = await getInfo(state.token);
+    if (re.code === 20000 || re.code === 200) {
+      const { name, avatar } = re.data;
+      commit("SET_NAME", name);
+      commit("SET_AVATAR", avatar);
+      return "ok";
+    } else {
+      return Promise.reject(new Error("error"));
+    }
   },
 
-  //get user info 的async/await写法
-  // async getInfo(miniStore, { state }) {
-  //   try {
-  //     const re = await getInfo(state.token);
-  //     const { data } = re;
-
-  //     if (!data) {
-  //       return reject("Verification failed, please Login again.");
-  //     }
-
-  //     const { name, avatar } = data;
-
-  //     miniStore.commit("SET_NAME", name);
-  //     miniStore.commit("SET_AVATAR", avatar);
-  //     return data;
-  //   } catch (e) {
-  //     return Promise.reject(e);
-  //   }
-  // },
-
-  // user logout 的async/await写法
-  logout({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      logout(state.token)
-        .then(() => {
-          removeToken(); // must remove  token  first
-          resetRouter();
-          commit("RESET_STATE");
-          resolve();
-        })
-        .catch(error => {
-          reject(error);
-        });
-    });
-  },
-
-  // user logout 的async/await写法
-  // async logout(miniStore, { state }) {
-  //   try {
-  //     await logout(state.token);
-  //     removeToken(); // must remove  token  first
-  //     resetRouter();
-  //     miniStore.commit("RESET_STATE");
-  //     resolve();
-  //   } catch (e) {
-  //     return Promise.reject(e);
-  //   }
-  // },
-
-  // remove token
-  resetToken({ commit }) {
-    return new Promise(resolve => {
-      removeToken(); // must remove  token  first
+  async logout({ commit, state }) {
+    const re = await logout(state.token);
+    if (re.code === 20000 || re.code === 200) {
+      localStorage.removeItem("token_key");
+      resetRouter();
       commit("RESET_STATE");
-      resolve();
-    });
+      return "ok";
+    } else {
+      return Promise.reject(new Error("error"));
+    }
+  },
+
+  async resetToken({ commit }) {
+    localStorage.removeItem("token_key");
+    commit("RESET_STATE");
   }
 };
 
 const mutations = {
   RESET_STATE: state => {
-    Object.assign(state, getDefaultState());
+    state.token = "";
+    state.name = "";
+    state.avatar = "";
   },
   SET_TOKEN: (state, token) => {
     state.token = token;
